d10af6842173e4a5d925bc8a6e6986c99df63380
lexomut <lexomut@mail.ru>
2021-09-10 21:40:34 +0300

    feat: add websocket message handler

--- src/realtime/WebSocketFunction.js
+++ src/realtime/WebSocketFunction.js
@@ -1,20 +1,44 @@
-import { wss } from "../index.js";
+import { handler } from "./handler.js";
+// eslint-disable-next-line no-unused-vars
 export function webSocketFunction(ws, req) {
-  console.log("соединение");
+  console.log("подключение c клиентом установлено");
+  // ws.send("сервер: вы подключились ко мне");
 
   ws.on("message", function (message) {
-    ws.send(message);
     console.log(message);
-  });
-  ws.on("close", () => {
-    console.log("WebSocket was closed");
-  });
-
-  // eslint-disable-next-line unicorn/prefer-add-event-listener
-  ws.onmessage = function (message) {
-    console.log(message.data);
-    for (const client of wss.clients) {
-      client.send(message.data);
+    try {
+      const mesg = JSON.parse(message);
+      switch (mesg.event) {
+        case "userConnection":
+          connectionHandler(
+            ws,
+            mesg,
+            handler.broadcastConnection.bind(handler)
+          );
+          break;
+        case "message":
+          connectionHandler(ws, mesg, handler.broadcastMessage.bind(handler));
+          break;
+        case "setGameState":
+          connectionHandler(ws, mesg, handler.setGameState.bind(handler));
+          break;
+      }
+    } catch {
+      ws.send("я вас не понимаю");
+      console.log("e");
     }
-  };
+
+    // console.log(message);
+  });
 }
+
+function connectionHandler(ws, mesg, callback) {
+  ws.id = mesg.gameID;
+  callback(ws, mesg);
+}
+
+// ws.on("close", () => {
+//   console.log("WebSocket was closed");
+// });
+
+// ws.onmessage = function (message) {}

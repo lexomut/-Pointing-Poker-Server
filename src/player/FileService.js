30e65764da72541baa73825f2f31a5c3d94c6135
lexomut <lexomut@mail.ru>
2021-09-30 19:17:36 +0300

    feat: add dir make

--- src/player/FileService.js
+++ src/player/FileService.js
@@ -1,10 +1,13 @@
 import * as path from "node:path";
 import * as uuid from "uuid";
+import * as fs from "node:fs";
 
 class FileService {
   saveFile(file) {
     const filename = uuid.v4() + "." + file.name.split(".")[1];
     try {
+      const directory = path.resolve("static");
+      fs.ensureDirSync(directory);
       const filePath = path.resolve("static", filename);
       file.mv(filePath);
     } catch (error) {
